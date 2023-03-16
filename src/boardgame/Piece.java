d3a80c2eba1306cf368a2cb3f4a23473d65aa7e7
jeanpereira3 <jeansilv13@gmail.com>
2023-03-16 10:41:27 -0300

    criadas class Board e Piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,17 @@
+package boardgame;
+
+public class Piece {
+	
+	private Board board;
+	protected Position position;
+	
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+	}
+
+	protected Board getBoard() {
+		return board;
+	}	
+	
+}
